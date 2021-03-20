6158b370c78cc64de7e8d2dd6f141a1f31ab6945
Herelius <youcefboutrig92@gmail.com>
2021-03-20 19:34:03 +0100

    jaden-case.js: OK

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -11,9 +11,24 @@
  */
 
 // Your code:
+function jadenCase(str) {
+    let output = [];
+    output[0] = str[0].toUpperCase();
+    for (let i = 1; i < str.length; i++) {
+        if (output[i-1] === ' ') {
+            output[i] = str[i].toUpperCase();
+        } else {
+            output[i] = str[i].toLowerCase();
+        }
+    };
+    return output.join("");
+};
 
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof jadenCase, 'function');
+assert.strictEqual(jadenCase.length, 1);
+assert.strictEqual(jadenCase('How are you ?'), 'How Are You ?')
+//assert.fail('You must write your own tests');
 // End of tests */
