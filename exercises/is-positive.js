4108443e3d5b6ec4710a1b9b32e333b3de89e456
Herelius <youcefboutrig92@gmail.com>
2021-03-20 14:49:25 +0100

    is-positive.js : OK

--- exercises/is-positive.js
+++ exercises/is-positive.js
@@ -8,6 +8,13 @@
  */
 
 // Your code:
+function isPositive(input) {
+    if (input > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 //* Begin of tests
 const assert = require('assert');
