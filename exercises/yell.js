7989193a5c8eb664947e91f7c6d193a56d27769e
Herelius <youcefboutrig92@gmail.com>
2021-03-20 16:59:14 +0100

    yell.js: OK

--- exercises/yell.js
+++ exercises/yell.js
@@ -7,9 +7,16 @@
  */
 
 // Your code:
+function yell(str) {
+    return str.toUpperCase();
+};
 
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof yell, 'function');
+assert.strictEqual(yell.length, 1);
+assert.strictEqual(yell('hello'), 'HELLO');
+assert.strictEqual(yell('hello world'), 'HELLO WORLD');
+// assert.fail('You must write your own tests');
 // End of tests */
