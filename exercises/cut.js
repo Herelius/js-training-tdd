bbaaa01d61c0e152a31f6bba8e62186af107b9a8
Herelius <youcefboutrig92@gmail.com>
2021-03-20 17:32:56 +0100

    cut.js: OK

--- exercises/cut.js
+++ exercises/cut.js
@@ -11,9 +11,29 @@
  */
 
 // Your code:
+function cutFirst(str) {
+    return str.substring(2);
+};
+function cutLast(str) {
+    return str.substring(0, str.length - 2);
+};
+function cutFirstLast(str) {
+    return str.substring(2, str.length - 2);
+};
 
 //* Begin of tests
 const assert = require('assert');
+assert.strictEqual(typeof cutFirst, 'function');
+assert.strictEqual(cutFirst.length, 1);
+assert.strictEqual(cutFirst('Hello world'), 'llo world');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof cutLast, 'function');
+assert.strictEqual(cutLast.length, 1);
+assert.strictEqual(cutLast('Hello world'), 'Hello wor');
+
+assert.strictEqual(typeof cutFirstLast, 'function');
+assert.strictEqual(cutFirstLast.length, 1);
+assert.strictEqual(cutFirstLast('Hello world'), 'llo wor');
+
+//assert.fail('You must write your own tests');
 // End of tests */
