a5c74120af53bb7a718e4ab0fd26e1472d8252d6
Herelius <youcefboutrig92@gmail.com>
2021-03-20 17:15:51 +0100

    whisper.js: OK

--- exercises/whisper.js
+++ exercises/whisper.js
@@ -8,9 +8,16 @@
  */
 
 // Your code:
+function whisper(str) {
+    return `*${str.toLowerCase()}*`;
+};
 
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof whisper, 'function');
+assert.strictEqual(whisper.length, 1);
+assert.strictEqual(whisper('Hello World'), '*hello world*');
+assert.strictEqual(whisper('HELLO WORLD ! '), '*hello world ! *');
+//assert.fail('You must write your own tests');
 // End of tests */
