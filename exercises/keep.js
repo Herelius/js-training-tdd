6a6c64a2438f96073250b79d1a4ec81ead68613d
Herelius <youcefboutrig92@gmail.com>
2021-03-20 18:40:13 +0100

    keep.js: OK

--- exercises/keep.js
+++ exercises/keep.js
@@ -13,9 +13,29 @@
  */
 
 // Your code:
+function keepFirst(str) {
+    return str.substring(0, 2);
+};
+function keepLast(str) {
+    return str.substring(str.length - 2, str.length);
+};
+function keepFirstLast(str) {
+    return str.substring(3, 5);
+};
 
 //* Begin of tests
 const assert = require('assert');
+assert.strictEqual(typeof keepFirst, 'function');
+assert.strictEqual(keepFirst.length, 1);
+assert.strictEqual(keepFirst('Hello world'), 'He');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof keepLast, 'function');
+assert.strictEqual(keepLast.length, 1);
+assert.strictEqual(keepLast('Hello world'), 'ld');
+
+assert.strictEqual(typeof keepFirstLast, 'function');
+assert.strictEqual(keepFirstLast.length, 1);
+assert.strictEqual(keepFirstLast('Hello world'), 'lo');
+
+//assert.fail('You must write your own tests');
 // End of tests */
